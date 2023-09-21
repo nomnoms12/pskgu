8687495035906ce17e43f6f38914baf5f39dfbf1
Alexander Ignatev <44947427+nomnoms12@users.noreply.github.com>
2023-09-21 08:52:35 +0300

    Update M. Високосный год.cpp

--- Программирование на языке C++/Сетевое образование 2023. Первое еженедельное задание/M. Високосный год.cpp
+++ Программирование на языке C++/Сетевое образование 2023. Первое еженедельное задание/M. Високосный год.cpp
@@ -3,5 +3,5 @@
 int main() {
   int year;
   std::cin >> year;
-  std::cout << (year % 4 == 0 && year % 100 != 0 || year % 400 == 0 ? "YES" : "NO");
+  std::cout << ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0 ? "YES" : "NO");
 }
