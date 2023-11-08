4a1dcf94b151cd479d3dcf502b66393299ee1163
nomnoms12 <44947427+nomnoms12@users.noreply.github.com>
2023-11-08 21:37:58 +0300

    Сетевое образование 2023. Седьмое еженедельное задание

--- Программирование на языке C++/Сетевое образование 2023. Седьмое еженедельное задание/D. Сортировка подсчетом.cpp
+++ Программирование на языке C++/Сетевое образование 2023. Седьмое еженедельное задание/D. Сортировка подсчетом.cpp
@@ -9,7 +9,7 @@ int main() {
     auto measurements = new int[n];
     for (int i = 0; i < n; i++) {
         std::cin >> measurements[i];
-        count[100 + measurements[i]]++;
+        count_sort[100 + measurements[i]]++;
     }
 
     for (int i = 0; i < 200; i++) {
